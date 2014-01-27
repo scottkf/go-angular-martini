d1ec4fe14e0558295d410c8d333d05fe2dc1ea76
Scott Tesoriere <scott@tesoriere.com>
2014-01-27 18:08:01 -0500

    easier to read

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"github.com/codegangsta/martini"
 	"github.com/codegangsta/martini-contrib/binding"
 	"github.com/codegangsta/martini-contrib/render"
+	"net/http"
 	"strconv"
 )
 
@@ -24,50 +25,58 @@ func main() {
 	m.Use(render.Renderer())
 
 	m.Get("/issues", func(r render.Render) {
-		r.JSON(200, data)
+		r.JSON(http.StatusOK, data)
 	})
 
 	m.Get("/issues/:id", func(r render.Render, params martini.Params) {
-		if params["id"] == "" {
-			r.JSON(404, nil)
+		id, err := Validate(params["id"])
+		if err > 0 {
+			r.JSON(err, nil)
+			return
 		}
-		id, err := strconv.Atoi(params["id"])
-		if err != nil {
-			r.JSON(500, nil)
-		}
-		r.JSON(200, data[id])
+		r.JSON(http.StatusOK, data[id])
 	})
 
 	m.Post("/issues", binding.Form(Issue{}), func(issue Issue, r render.Render, params martini.Params) {
 		data = append(data, issue)
-		r.JSON(200, issue)
+		r.JSON(http.StatusOK, issue)
 	})
 
 	m.Put("/issues/:id", binding.Form(Issue{}), func(issue Issue, r render.Render, params martini.Params) {
-		if params["id"] == "" {
-			r.JSON(404, nil)
-		}
-		id, err := strconv.Atoi(params["id"])
-		if err != nil {
-			r.JSON(500, nil)
+		id, err := Validate(params["id"])
+		if err > 0 {
+			r.JSON(err, nil)
+			return
 		}
 		i := &data[id]
 		i.Title = issue.Title
 		i.Body = issue.Body
-		r.JSON(200, i)
+		r.JSON(http.StatusOK, i)
 	})
 
 	m.Delete("/issues/:id", func(r render.Render, params martini.Params) {
-		if params["id"] == "" {
-			r.JSON(404, nil)
-		}
-		id, err := strconv.Atoi(params["id"])
-		if err != nil {
-			r.JSON(500, nil)
+		id, err := Validate(params["id"])
+		if err > 0 {
+			r.JSON(err, nil)
+			return
 		}
 		data = append(data[:id], data[id+1:]...)
-		r.JSON(200, nil)
+		r.JSON(http.StatusOK, nil)
 	})
 
 	m.Run()
+}
+
+func Validate(id string) (int, int) {
+	if id == "" {
+		return -1, http.StatusNotFound
+	}
+	i, err := strconv.Atoi(id)
+	if err != nil {
+		return -1, http.StatusInternalServerError
+	}
+	if i >= len(data) {
+		return -1, http.StatusBadRequest
+	}
+	return i, -1
 }
